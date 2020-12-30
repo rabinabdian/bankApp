f6ae744d5afcf9ddf6a58c2f46600aa5dc7d6640
rabinabdian <rabin.abdian@gmail.com>
2020-12-30 13:35:25 +0200

    add info acc type

--- src/main/java/Savings.java
+++ src/main/java/Savings.java
@@ -1,17 +1,34 @@
 public class Savings extends Account{
+
+    // props
+    int safetyDepositBoxID;
+    int safetyDepositBoxKEY;
+
+
+
     public Savings(String name, String sSN, double initDeposit) {
         super(name, sSN, initDeposit);
-//        System.out.println("NEW SAVING ACCOUNT");
+
+        accountNumber = "1" + accountNumber;
+
+        setSafetyDepositBox();
+
     }
 
-
-
+    private void setSafetyDepositBox() {
+        safetyDepositBoxID = (int)(Math.random() * Math.pow(10,3));
+        safetyDepositBoxKEY = (int)(Math.random() * Math.pow(10,4));
+    }
 
 
     @Override
     public void showInfo() {
         super.showInfo();
-        System.out.println("ACCOUNT TYPE: SAVING");
+        System.out.println(
+                " Saving Account Features "+
+                        "\n Safety Deposit Box ID: "+ safetyDepositBoxID +
+                        "\n Safety Deposit Box KEY: "+ safetyDepositBoxKEY
+        );
         System.out.println("***********************");
     }
 }
